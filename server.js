6f862dd86119e3be4b1ff50d36fc1e43fe03a3bc
austintedwards <austintedwards@gmail.com>
2017-10-01 12:29:58 -0600

    first commit

---
+++ server.js
@@ -0,0 +1,34 @@
+var express = require("express");
+var app = express();
+var bodyParser = require("body-parser");
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.use(express.static("public"));
+
+app.use(function(req, res, next) {
+    res.status(404);
+    res.send('404 file not found!');
+});
+
+
+app.use(function(err, req, res , next) {
+    console.log(err);
+    res.status(500);
+    res.send('500 internal server error');
+    res.send(err);
+});
+
+function startListening() {
+    app.listen(8000, function() {
+        console.log("server is started: http://localhost:8000 ⚡️");
+    });
+}
+
+startListening();
+
+
+// module.exports = app
